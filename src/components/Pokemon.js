69315a78fddf1cc1bcbbe7b8fa050a5472805a1c
Aurore <aurorem@bam.tech>
2018-06-03 18:16:55 +0200

    Display pokemon on a plan

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -4,12 +4,35 @@ import React, { Component } from 'react';
 
 import { StyleSheet } from 'react-native';
 
-import { Viro3DObject, ViroAmbientLight, ViroNode } from 'react-viro';
+import { Viro3DObject, ViroAmbientLight, ViroARPlane } from 'react-viro';
 
 export default class Pokemon extends Component {
+  constructor() {
+    super();
+    this.state = {
+      planeCoords: {
+        x: 0,
+        y: 0,
+        z: 0,
+      },
+    };
+  }
   render() {
     return (
-      <ViroNode position={[0.0, 0.0, -10]} rotation={[-90, 0, 0]}>
+      <ViroARPlane
+        minHeight={0.05}
+        minWidth={0.05}
+        alignment={'Horizontal'}
+        onAnchorFound={e => {
+          this.setState({
+            planeCoords: {
+              x: e.position[0],
+              y: e.position[1],
+              z: e.position[2] - 1,
+            },
+          });
+        }}
+      >
         <ViroAmbientLight color="#FFFFFF" />
         <Viro3DObject
           source={require('PokeBAM/src/assets/3D/Pokemons/Squirtle/Squirtle.vrx')}
@@ -22,10 +45,13 @@ export default class Pokemon extends Component {
             require('PokeBAM/src/assets/3D/Pokemons/Squirtle/pm0007_00_Eye2.png'),
             require('PokeBAM/src/assets/3D/Pokemons/Squirtle/pm0007_00_EyeNor.png'),
           ]}
-          scale={[0.1, 0.1, 0.1]}
+          position={[this.state.planeCoords.x, this.state.planeCoords.y, this.state.planeCoords.z]}
+          rotation={[-90, 0, 0]}
+          dragType="FixedToWorld"
+          scale={[0.015, 0.015, 0.015]}
           type="VRX"
         />
-      </ViroNode>
+      </ViroARPlane>
     );
   }
 }
